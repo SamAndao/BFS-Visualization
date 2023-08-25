93dee90a04850c256a107d21fc08a073b71e30d2
SamAndao <andaosam@gmail.com>
2023-08-25 20:45:17 +0800

    fixed paths

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/DFS-BFS-Visualization" Component={<App />}></Route>
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
